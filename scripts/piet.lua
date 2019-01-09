e69a3e24225514fba24f5fe0548123954ed52cb0
edward matyja <ekmatyja98@gmail.com>
2019-01-09 04:19:44 -0500

    added comments to describe the particle system

--- scripts/piet.lua
+++ scripts/piet.lua
@@ -16,13 +16,13 @@ function piet:load()
     local img = love.graphics.newImage("/assets/red.png")
 
     psystem = love.graphics.newParticleSystem(img, 10)
-    psystem:setParticleLifetime(2, 5)
-    psystem:setEmissionRate(5)
+    psystem:setParticleLifetime(2, 5) --used to denote time on screen from minimum time alive to maximum
+    psystem:setEmissionRate(5) --speed at which particles get produced
     psystem:setSizeVariation(1)
-    psystem:setLinearAcceleration(-20, -20, 20, 0)
-    psystem:setSpeed(5, 10)
+    psystem:setLinearAcceleration(-20, -20, 20, 0) --used to set boundaries for particle movement. (x min, x max, y min, y max)
+    psystem:setSpeed(5, 10) --sets speed at which particles travel per unit of time. ranges from slowest to fastest
     psystem:setColors(255, 255, 255, 255, 255, 255, 255, 0)
-    psystem:moveTo(self.x, self.y)
+    psystem:moveTo(self.x, self.y) --defines where the emission should move to when updated. this is tied to the position of the player at all times
 
     self.body = love.physics.newBody(world.world, self.x * 32, self.y * 32, "dynamic", 0, 100)
     self.body:setLinearDamping(.5);
